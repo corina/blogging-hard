548011962462d7fb2c40f4bc9136149201d8cf8f
corina <corina.gheorghe@staffordlong.co.uk>
2018-04-03 12:04:36 +0100

    Add validations to Posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+
+	validates :title, 			 :presence => true,
+													 :length => { :minimum => 5, :maximum => 40}
+	validates :introduction, :presence => true,
+													 :length => { :minimum => 5}			
+	validates :body, 				 :presence => true,
+										       :length => { :minimum => 5}																
 end
